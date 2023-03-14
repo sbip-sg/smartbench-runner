c8ca21024fc8552635354a6651dfe62f0e9d5790
Ta Quang Trung <taquangtrungvn@gmail.com>
2023-03-14 21:05:24 +0800

    rectify checking locations when validating issues

--- smartbench/validate.py
+++ smartbench/validate.py
@@ -57,9 +57,11 @@ def match_issue_to_annotation(issue: Issue, annot: BugAnnot) -> bool:
         return False
 
     # Check whether the issue location is covered by the annotation location.
-    if iloc.start_line is not None and iloc.start_line < annot.start_line:
+    if iloc.start_line is None or iloc.end_line is None:
         return False
-    if iloc.end_line is not None and iloc.end_line > annot.end_line:
+    if iloc.start_line < annot.start_line:
+        return False
+    if iloc.end_line > annot.end_line:
         return False
 
     # Pass all criteria to match an issue with a bug annotation
