36655b67d9fdd8e477ae285e3b231e4c2b0c91e7
Nguyen Thanh Toan <thanhtoantnt@gmail.com>
2023-03-17 20:56:43 +0800

    using tomli instead of toml (#14)

--- smartbench/log.py
+++ smartbench/log.py
@@ -7,7 +7,7 @@
 from typing import Union
 
 # Third Party
-import toml
+import tomli
 
 # Library
 from smartbench.debug import warning
@@ -17,7 +17,7 @@ def get_input_test_file(log_file: str) -> Union[str, None]:
     """Get input test file from a log file"""
     with open(log_file, "r", encoding="utf-8") as file:
         file_content = file.read()
-        log = toml.loads(file_content)
+        log = tomli.loads(file_content)
         input_log = log.get("input")
         if input_log is None:
             warning("Invalid log file! No input information is found!")
