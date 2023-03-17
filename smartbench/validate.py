53fce280dfd055e9d570df11d9d63381a6e0a9c6
thanhtoantnt <thanhtoantnt@gmail.com>
2023-03-17 17:32:43 +0800

    update validation steps

--- smartbench/validate.py
+++ smartbench/validate.py
@@ -59,9 +59,9 @@ def match_issue_to_annotation(issue: Issue, annot: BugAnnot) -> bool:
     # Check whether the issue location is covered by the annotation location.
     if iloc.start_line is None or iloc.end_line is None:
         return False
-    if iloc.start_line < annot.start_line:
+    if iloc.start_line < annot.start_line + 1:
         return False
-    if iloc.end_line > annot.end_line:
+    if iloc.end_line > annot.end_line + 1:
         return False
 
     # Pass all criteria to match an issue with a bug annotation
