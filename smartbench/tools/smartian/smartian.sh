50cbc6ac6d5b6749f07b947148dc67103e72d956
thanhtoantnt <thanhtoantnt@gmail.com>
2023-03-29 16:44:21 +0800

    update running smartian

--- smartbench/tools/smartian/smartian.sh
+++ smartbench/tools/smartian/smartian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 FILENAME="$1"
-TIMEOUT="$2"
+TOTAL_TIMEOUT="$2"
 VERSION="$3"
 
 TOOL_ID="smartian"
@@ -9,19 +9,22 @@ BASE_DIR=$(dirname "$0")
 REPO_DIR=$BASE_DIR"/repo"
 TOOL_DIR=$REPO_DIR/$TOOL_ID
 
-cd $TOOL_DIR
-mkdir output
-mkdir input
-solc-select install $VERSION
-solc-select use $VERSION
-
 CONTRACT="${FILENAME%.sol}"
 CONTRACT="${CONTRACT##*/}"
 CONTRACTS=$(python3 smartbench/tools/smartian/printContractNames.py "$FILENAME")
 COUNT=$(echo $CONTRACTS | wc -w)
-EACH_TIMEOUT = $TIMEOUT/$COUNT
+TIMEOUT=$((TOTAL_TIMEOUT / COUNT))
+
+cd $TOOL_DIR
+rm -rf input
+rm -rf output
+mkdir output
+mkdir input
+solc-select install $VERSION
+solc-select use $VERSION
+solc --bin --abi $1 -o input --overwrite
 
 for CONTRACT in $CONTRACTS; do
-    dotnet build/Smartian.dll fuzz -p input/$CONTRACT.bin  -a input/$CONTRACT. -t $EACH_TIMEOUT -o output
+    dotnet build/Smartian.dll fuzz --program input/$CONTRACT.bin --abifile input/$CONTRACT.abi -t $TIMEOUT -o output
 done
 
