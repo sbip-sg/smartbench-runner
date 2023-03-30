c9215d8681fb89b21530e3a45955b7d673deed27
thanhtoantnt <thanhtoantnt@gmail.com>
2023-03-31 00:52:41 +0800

    update cli arguments

--- smartbench/cli.py
+++ smartbench/cli.py
@@ -15,6 +15,7 @@ class Command(Enum):
 
     ANALYZE = "analyze"
     PARSE_RESULTS = "parse-results"
+    PARSE_COVERAGE = "parse-coverage"
     PARSE_ANNOTS = "parse-annots"
     DEPLOY_CONTRACTS = "deploy-contracts"
 
@@ -143,6 +144,25 @@ def parse_cli_arguments():
         "--validate-results",
         action="store_true",
         help="Validate analysis results with bug annotations.",
+    )
+
+    ################################
+    # Parser for sub-command `parse-coverage`
+
+    # Create a parser for the `parse-coverage` sub-command
+    coverage_parser = sub_parsers.add_parser(
+        Command.PARSE_COVERAGE.value,
+        parents=[parent_parser],
+        add_help=False,
+        help="Sub-command to parse instruction coverage in existing analysis results.",
+    )
+
+    # Input result directories
+    coverage_parser.add_argument(
+        "result_directories",
+        nargs="+",  # Accept multiple input files or directories
+        type=str,
+        help="Input result directories.",
     )
 
     ################################
