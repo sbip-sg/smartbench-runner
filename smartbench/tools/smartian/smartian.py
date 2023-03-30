e7b7184b6341e8f6414c3d75841a90d475c30c44
thanhtoantnt <thanhtoantnt@gmail.com>
2023-03-30 13:02:04 +0800

    update timeout

--- smartbench/tools/smartian/smartian.py
+++ smartbench/tools/smartian/smartian.py
@@ -39,8 +39,9 @@ def make_analysis_command(
 
     solc_version = os.path.basename(solc_path)
     solc_version = solc_version.removeprefix("solc-")
-    # TODO: To merge with the timeout from the `tool.py`
-    timeout = 30
+
+    if timeout is None:
+        timeout = 60
 
     command = (
         command
