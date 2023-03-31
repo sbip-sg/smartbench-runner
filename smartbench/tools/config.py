51e8c0b428488b0e989e683b80c835ec8b5ab055
thanhtoantnt <thanhtoantnt@gmail.com>
2023-03-31 16:41:16 +0800

    fix results directory

--- smartbench/tools/config.py
+++ smartbench/tools/config.py
@@ -34,7 +34,8 @@ DEFAULT_ARGUMENTS = "default_arguments"
 TOOLS_DIR = os.path.dirname(__file__)
 SMARTBENCH_ROOT_DIR = os.path.dirname(os.path.dirname(TOOLS_DIR))
 # print(f"SMARTBENCH_ROOT_DIR: {SMARTBENCH_ROOT_DIR}")
-RESULTS_DIR = os.path.join(os.path.dirname(SMARTBENCH_ROOT_DIR), "results")
+RESULTS_DIR = os.path.join(SMARTBENCH_ROOT_DIR, "results")
+# print(f"RESULTS_DIR: {RESULTS_DIR}")
 DEPLOY_DIR = os.path.join(os.path.dirname(SMARTBENCH_ROOT_DIR), "deploy")
 
 
