dc198a3465fca542b862d737ac0a7db86464301c
Ta Quang Trung <taquangtrungvn@gmail.com>
2023-04-06 01:43:40 +0800

    add script to run Confuzzius in Docker

--- smartbench/tools/confuzzius/run-confuzzius-docker.sh
+++ smartbench/tools/confuzzius/run-confuzzius-docker.sh
@@ -13,20 +13,10 @@
 TEST_FILE=$(realpath $1)
 OTHER_ARGS=${@:2}
 
-# Tool settings
-TOOL_ID="confuzzius"
-
-# Prepare repository directory
-TOOL_DIR=$(realpath $(dirname "$0"))
-TOOL_SRC_DIR=$TOOL_DIR"/repo/confuzzius"
-
-# Configure venv
-. $TOOL_DIR/confuzzius_venv/bin/activate
-
 # Detect Solc version to be used.
 SOLC_VER=$(solc-detect $TEST_FILE)
 
 # Run Confuzzius
-SOLC_VERSION=$SOLC_VER python $TOOL_SRC_DIR/fuzzer/main.py \
+SOLC_VERSION=$SOLC_VER python /root/confuzzius/fuzzer/main.py \
     --evm byzantium \
     -s $TEST_FILE $OTHER_ARGS
