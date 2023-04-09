f1ccd5c1524b4785e8f1679ae2a767218dc960d7
Ta Quang Trung <taquangtrungvn@gmail.com>
2023-04-10 03:12:53 +0800

    configure sFuzz for Docker

--- smartbench/tools/sfuzz/run-sfuzz.sh
+++ smartbench/tools/sfuzz/run-sfuzz.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Usage:
-#   ./run-smartian.sh <test-file> [-c <contract names>] [additional-smartian-arguments]
+#   ./run-sfuzz.sh <test-file> [-c <contract names>] [additional-sfuzz-arguments]
 #
 # NOTE:
 #   - Test file must be the first argument
@@ -10,7 +10,7 @@
 #     in a Docker container as well as to run locally.
 #
 
-# Arguments of Smartian
+# Arguments of sFuzz
 TEST_FILE=$(realpath $1)
 shift  # Past test file
 
@@ -57,49 +57,49 @@ done
 
 # Checking output dir
 if [[ $OUTPUT_DIR == "" ]]; then
-    echo "Smartian: output dir is not specified!"
+    echo "sFuzz: output dir is not specified!"
     exit 1
 fi
 
 # Checking timeout
 if [[ $TIMEOUT -lt 0 ]]; then
-    echo "Smartian: timeout is not specified or invalid!"
+    echo "sFuzz: timeout is not specified or invalid!"
     exit 1
 fi
 
 # Configure tool path when running inside or outside a Docker container.
 if [ -f /.dockerenv ]; then
-    TOOL_ROOT_PATH="/root/smartian"
+    TOOL_ROOT_PATH="/root/sfuzz"
 else
-    TOOL_ROOT_PATH="$(realpath $(dirname "$0"))/repo/smartian"
+    TOOL_ROOT_PATH="$(realpath $(dirname "$0"))/repo/sfuzz"
 fi
 
 # Detect Solc version to be used.
 SOLC_VER=$(solc-detect $TEST_FILE)
 
-# # Compile test file to contracts in ABI and BIN format
-# CONTRACTS_DIR="$OUTPUT_DIR/compiled_contracts"
-# rm -rf $CONTRACTS_DIR
-# mkdir $CONTRACTS_DIR
-# SOLC_VERSION=$SOLC_VER solc $TEST_FILE --bin --abi \
-#     -o $CONTRACTS_DIR --overwrite \
-#     1>/dev/null 2>&1  # Do not capture output of Solc
+# Compile test file to contracts in ABI and BIN format
+CONTRACTS_DIR="$OUTPUT_DIR/compiled_contracts"
+rm -rf $CONTRACTS_DIR
+mkdir $CONTRACTS_DIR
+SOLC_VERSION=$SOLC_VER solc $TEST_FILE --bin --abi \
+    -o $CONTRACTS_DIR --overwrite \
+    1>/dev/null 2>&1  # Do not capture output of Solc
 
-# if [[ ${#CONTRACT_NAMES[@]}  == 0 ]]; then
-#     CURRENT_DIR=$(pwd)
-#     cd $CONTRACTS_DIR
-#     CONTRACT_NAMES=($(ls -1 *.bin | sed "s/\.bin//"))
-#     cd $CURRENT_DIR
-# fi
+if [[ ${#CONTRACT_NAMES[@]}  == 0 ]]; then
+    CURRENT_DIR=$(pwd)
+    cd $CONTRACTS_DIR
+    CONTRACT_NAMES=($(ls -1 *.bin | sed "s/\.bin//"))
+    cd $CURRENT_DIR
+fi
 
-# # Run Smartian on each candidate contract
-# for CONTRACT in ${CONTRACT_NAMES[@]}; do
-#     echo "==============================="
-#     echo "** Fuzzing contract: $CONTRACT"
-#     dotnet $TOOL_ROOT_PATH/build/Smartian.dll fuzz \
-#         --useothersoracle --checkoptionalbugs --verbose 1 \
-#         --program "$CONTRACTS_DIR/$CONTRACT.bin" \
-#         --abifile "$CONTRACTS_DIR/$CONTRACT.abi" \
-#         --outputdir $OUTPUT_DIR --timelimit $TIMEOUT \
-#         ${ADDITIONAL_ARGS[@]} 2>&1
-# done
+# Run sFuzz on each candidate contract
+for CONTRACT in ${CONTRACT_NAMES[@]}; do
+    echo "==============================="
+    echo "** Fuzzing contract: $CONTRACT"
+    dotnet $TOOL_ROOT_PATH/build/fuzzer -g \
+        --useothersoracle --checkoptionalbugs --verbose 1 \
+        --program "$CONTRACTS_DIR/$CONTRACT.bin" \
+        --abifile "$CONTRACTS_DIR/$CONTRACT.abi" \
+        --outputdir $OUTPUT_DIR --timelimit $TIMEOUT \
+        ${ADDITIONAL_ARGS[@]} 2>&1
+done
---
+++ smartbench/tools/sfuzz/sfuzz.Dockerfile
@@ -0,0 +1,25 @@
+# Dockerfile for sFuzz
+
+# Usage: this file should only be run by the installation script:
+# `smartbench-runner/install-tool-docker.sh`
+
+# Use the base image of Smartbench
+FROM smartbench/base:latest
+
+# Install sFuzz dependencies
+RUN apt -y install cmake libleveldb-dev
+
+# Install sFuzz
+WORKDIR /root/
+RUN git clone --recursive https://github.com/thanhtoantnt/sFuzz.git sfuzz
+WORKDIR /root/sfuzz
+RUN mkdir -p build; cd build; cmake ..
+WORKDIR /root/sfuzz/build/fuzzer
+RUN make
+
+# Copy executable file
+# ADD smartbench/tools/sfuzz/run-confuzzius.sh /root/
+
+# Entry point when running the container as an executable
+WORKDIR /root/
+ENTRYPOINT [ "/bin/bash" ]
