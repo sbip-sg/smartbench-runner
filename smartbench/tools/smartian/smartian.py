3719c48081b89052f0a2868b78314956592d3611
thanhtoantnt <thanhtoantnt@gmail.com>
2023-04-12 14:36:11 +0800

    update with percentage

--- smartbench/tools/smartian/smartian.py
+++ smartbench/tools/smartian/smartian.py
@@ -243,8 +243,18 @@ class Smartian(Tool):
         current_time = 0
 
         contracts_coverage = []
+        total_execution = 0
 
         for line in lines:
+            execution_match = re.search(r"Total Executions: [0-9]+", line)
+            if execution_match is not None:
+                execution_num = execution_match.group();
+                execution_num = execution_num.removeprefix(
+                            "Total Executions: "
+                )
+                if int(execution_num) > 0:
+                    total_execution += int(execution_num)
+
             match_str = re.search(r"Covered Instructions: [0-9]+", line)
             time_str = re.search(
                 "(\d{2})[/.:](\d{2})[/.:](\d{2})[/.:](\d{2})", line
@@ -270,13 +280,16 @@ class Smartian(Tool):
         contracts_coverage.append(coverages)
 
         current_time = 0
-        instruction = 0
+        current_instr = 0
         results = []
+        print(f"total: ", total_execution)
         for contract_coverage in contracts_coverage:
             for (time, instr) in contract_coverage:
-                instruction += instr
-                results.append((time + current_time, instruction))
+                instr += current_instr
+                percentage = float("{:.1f}".format(instr / total_execution))
+                results.append((time + current_time, percentage))
 
             current_time += contract_coverage[-1][0]
+            current_instr += contract_coverage[-1][1]
 
         return results
