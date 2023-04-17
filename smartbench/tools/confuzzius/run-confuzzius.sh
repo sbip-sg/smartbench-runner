ca7c66577cdc92f2f0bf7394a0aecbee3e91e3ba
thanhtoantnt <thanhtoantnt@gmail.com>
2023-04-17 17:35:34 +0800

    update run-confuzzius script

--- smartbench/tools/confuzzius/run-confuzzius.sh
+++ smartbench/tools/confuzzius/run-confuzzius.sh
@@ -14,6 +14,7 @@ print_usage () {
     echo ""
     echo "Options:"
     echo "  -f <test-file>        Smart contract file to be analyzed."
+    echo "  -c <contract-names>   Names of contracts to be analyzed (whitespace separated)."
     echo "  -h, --help            Print this usage."
     echo ""
     echo "Addtional arguments passing to Confuzzius can be put at the end of this command."
@@ -28,6 +29,7 @@ print_help () {
 # Parse arguments
 
 TEST_FILE=""
+CONTRACT_NAMES=()
 ADDITIONAL_ARGS=()
 
 while [[ $# -gt 0 ]]; do
@@ -36,6 +38,21 @@ while [[ $# -gt 0 ]]; do
             TEST_FILE=$(realpath $2)
             shift # past argument
             shift # past value
+            ;;
+        -c)
+            shift # past argument
+            # Parse contract names
+            while [[ $# -gt 0 ]]; do
+                case $1 in
+                    -*|--*)
+                        break
+                        ;;
+                    *)
+                        CONTRACT_NAMES+=("$1")
+                        shift  # past value
+                        ;;
+                esac
+            done
             ;;
         -h|--help)
             print_usage
@@ -55,6 +72,15 @@ if [[ $TEST_FILE == "" ]]; then
     exit 1
 fi
 
+# If contract names are not specified from the input, analyze all contracts
+# obtained after compilation.
+if [[ ${#CONTRACT_NAMES[@]}  == 0 ]]; then
+    CURRENT_DIR=$(pwd)
+    cd $COMPILED_CONTRACTS_DIR
+    CONTRACT_NAMES=($(ls -1 *.bin | sed "s/\.bin//"))
+    cd $CURRENT_DIR
+fi
+
 ################################################
 # Analyze test file
 
@@ -68,6 +94,10 @@ fi
 # Detect Solc version to be used.
 SOLC_VER=$(solc-detect $TEST_FILE)
 
-# Run Confuzzius
-SOLC_VERSION=$SOLC_VER python "$TOOL_DIR/fuzzer/main.py" --evm byzantium \
-    -s $TEST_FILE ${ADDITIONAL_ARGS[@]} 2>&1
+# Run Confuzzius on each candidate contract
+for CONTRACT in ${CONTRACT_NAMES[@]}; do
+    echo "==============================="
+    echo "** Fuzzing contract: $CONTRACT"
+    SOLC_VERSION=$SOLC_VER python "$TOOL_DIR/fuzzer/main.py" --evm byzantium \
+                           -c $CONTRACT -s $TEST_FILE ${ADDITIONAL_ARGS[@]} 2>&1
+done
