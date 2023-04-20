7c7fea7238f0c22a4a009fe352ae8f91c7ff4717
thanhtoantnt <thanhtoantnt@gmail.com>
2023-04-20 14:46:42 +0800

    update timeout

--- smartbench/tools/confuzzius/confuzzius.py
+++ smartbench/tools/confuzzius/confuzzius.py
@@ -76,7 +76,8 @@ class Confuzzius(Tool):
         # additional arguments of Confuzzius
         if self.additional_args is None or "-t " not in self.additional_args:
             timeout = self.default_timeout if timeout is None else timeout
-            cmd = cmd + " -t " + str(timeout)
+            contract_timeout = math.ceil(timeout / len(contracts))
+            cmd = cmd + " -t " + str(contract_timeout)
 
         # Finally, pass default and additional arguments
         if self.default_arguments:
