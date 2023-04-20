df773e84258beb01371648c82991f63b269f430e
Ta Quang Trung <taquangtrungvn@gmail.com>
2023-04-20 17:06:22 +0800

    add library

---
+++ smartbench/process.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Module containing utilities handling processes"""
+
+# Standard Library
+import os
+import signal
+
+from smartbench.printer import safe_print
+
+
+def ignore_sigint() -> None:
+    """Ignore SIGINT signal"""
+    print("== IGNORE SIGINT")
+    os.setpgrp()
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
