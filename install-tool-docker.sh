543a1c30dc8e36a9696690ae8d858c8afb1e1134
Ta Quang Trung <taquangtrungvn@gmail.com>
2023-04-26 17:30:26 +0800

    update script to build docker containers for multiple tools

--- install-tool-docker.sh
+++ install-tool-docker.sh
@@ -4,17 +4,20 @@
 
 SUPPORTED_TOOLS=("slither" "sfuzz" "confuzzius" "smartian" "smartfuzz" "ilf" "mythril")
 
+SUPPORTED_TOOL_IDS=${SUPPORTED_TOOLS[@]}
+SUPPORTED_TOOL_IDS+=("all")
+
 ################################################
 # Usage
 
 print_usage () {
     echo ""
     echo "Usage: "
-    echo "  install-tool-docker.sh -t <tool-id> [options] [container_1, ... , container_n]"
+    echo "  install-tool-docker.sh -t <tool-ids> [options] [container_1, ... , container_n]"
     echo ""
     echo "Options:"
     echo "  -t <tool-id>               ID of analysis tool, currently support the followings:"
-    echo "                             $(echo ${SUPPORTED_TOOLS[@]} | sed 's/ /, /g') "
+    echo "                             $(echo ${SUPPORTED_TOOL_IDS[@]} | sed 's/ /, /g') "
     echo "  -n <number_of_containers>  Number of containers to be installed, which are named"
     echo "                             as {tool-id}-1, {tool-id}-2,..., {tool-id}-n."
     echo "  --force-install            Force install new containers."
@@ -76,10 +79,18 @@ if [[ $TOOL_ID == "" ]]; then
     echo "Error: analysis tool ID is not specified!"
     print_run_help
     exit 1
-elif [[ ! $(echo ${SUPPORTED_TOOLS[@]} | grep -w $TOOL_ID) ]]; then
+elif [[ ! $(echo ${SUPPORTED_TOOL_IDS[@]} | grep -w $TOOL_ID) ]]; then
     echo "Error: tool $TOOL_ID is not supported!"
     print_run_help
     exit 1
+fi
+
+# Getting IDs of all tools to be built
+ALL_TOOL_IDS=()
+if [[ $TOOL_ID == "all" ]]; then
+    ALL_TOOL_IDS=(${SUPPORTED_TOOLS[@]})
+else
+    ALL_TOOL_IDS=($TOOL_ID)
 fi
 
 # Checking container names
@@ -89,38 +100,14 @@ if [[ $NUM_CONTAINERS == "" ]]; then
     exit 1
 fi
 
-for ((i=1; i<=$NUM_CONTAINERS; i++)); do
-    CONTAINER_NAMES+=("$TOOL_ID-$i")
-done
-
-if [[ ${#CONTAINER_NAMES[@]} == 0 ]]; then
-    echo "Error: no container name or number of container is specified!"
+if [[ ${#ALL_TOOL_IDS[@]} > 1 && ${#CONTAINER_NAMES[@]} > 0 ]]; then
+    echo "Do not specify container name '${CONTAINER_NAMES[@]}' when building for multiple tools!"
     print_run_help
     exit 1
 fi
 
-
 ################################################
-
-# Smartbench directories
-SMARTBENCH_ROOT=$(realpath $(dirname "$0"))
-SMARTBENCH_BENCHMARKS_DIR="$SMARTBENCH_ROOT/benchmarks"
-SMARTBENCH_EXAMPLES_DIR="$SMARTBENCH_ROOT/examples"
-SMARTBENCH_RESULTS_DIR="$SMARTBENCH_ROOT/results"
-SMARTBENCH_DOCKER_FILE="$SMARTBENCH_ROOT/smartbench/tools/smartbench.Dockerfile"
-SMARTBENCH_DOCKER_IMAGE="smartbench/base"
-
-# Tool directories
-TOOL_DIR="$SMARTBENCH_ROOT/smartbench/tools/$TOOL_ID"
-
-# Docker information
-TOOL_DOCKER_FILE="$TOOL_DIR/$TOOL_ID.Dockerfile"
-TOOL_DOCKER_IMAGE="smartbench/$TOOL_ID"
-
-# Docker container directories
-DOCKER_BENCHMARKS_DIR="/root/benchmarks"
-DOCKER_EXAMPLES_DIR="/root/examples"
-DOCKER_RESULTS_DIR="/root/results"
+# Handling errors
 
 # Quit on error during installation
 set -e
@@ -143,69 +130,116 @@ clean_up () {
 # Clean up when error occur
 trap "clean_up 1" ERR
 
-echo "============================================="
-echo "Install $TOOL_ID in docker mode"
-echo "Prepare environments..."
+################################################
 
-# Build Docker image
+# Smartbench directories
+SMARTBENCH_ROOT=$(realpath $(dirname "$0"))
+SMARTBENCH_BENCHMARKS_DIR="$SMARTBENCH_ROOT/benchmarks"
+SMARTBENCH_EXAMPLES_DIR="$SMARTBENCH_ROOT/examples"
+SMARTBENCH_RESULTS_DIR="$SMARTBENCH_ROOT/results"
+SMARTBENCH_DOCKER_FILE="$SMARTBENCH_ROOT/smartbench/tools/smartbench.Dockerfile"
+SMARTBENCH_DOCKER_IMAGE="smartbench/base"
+
+# Docker container directories
+DOCKER_BENCHMARKS_DIR="/root/benchmarks"
+DOCKER_EXAMPLES_DIR="/root/examples"
+DOCKER_RESULTS_DIR="/root/results"
+
+# Build base Docker image
+echo "Start building docker containers for analysis tools... "
+echo ""
 echo "============================================="
 echo "Building base image for all analysis tools..."
+echo ""
 
 cd $SMARTBENCH_ROOT
 docker build -f $SMARTBENCH_DOCKER_FILE -t $SMARTBENCH_DOCKER_IMAGE .
 
-# Build Docker image
+echo ""
 echo "============================================="
-echo "Building Docker image for $TOOL_ID..."
+echo "Start building docker container(s) for: ${ALL_TOOL_IDS[@]}"
+echo ""
 
-if [[ $USE_GIT_TOKEN == true ]]; then
-    echo "Git Access Token is required to build Docker image from: $TOOL_DOCKER_FILE"
-    echo -n "Enter your Git Access Token: "
-    read GIT_TOKEN
-    docker build -f $TOOL_DOCKER_FILE -t $TOOL_DOCKER_IMAGE --build-arg GIT_ACCESS_TOKEN=$GIT_TOKEN .
-else
-    docker build -f $TOOL_DOCKER_FILE -t $TOOL_DOCKER_IMAGE .
-fi
+for TOOL_ID in ${ALL_TOOL_IDS[@]}; do
 
-# Create a new Docker container that share the two folders:
-# `benchmarks` and `results` with the host system.
-echo "============================================="
-echo "Creating docker containers: ${CONTAINER_NAMES[*]}"
+    # Tool directories
+    TOOL_DIR="$SMARTBENCH_ROOT/smartbench/tools/$TOOL_ID"
 
-if $FORCE_INSTALL; then
-    echo "Running force-install mode"
-fi
+    # Docker information
+    TOOL_DOCKER_FILE="$TOOL_DIR/$TOOL_ID.Dockerfile"
+    TOOL_DOCKER_IMAGE="smartbench/$TOOL_ID"
 
-for CONTAINER in "${CONTAINER_NAMES[@]}"; do
+    # Build Docker image
+    echo "============================================="
+    echo "Building Docker image for: $TOOL_ID..."
     echo ""
-    echo "Checking container: $CONTAINER"
-    if [[ $(docker ps -a -f name=$CONTAINER | grep -e "[ \t]$CONTAINER\$") ]]; then
-        if $FORCE_INSTALL; then
-            echo "A container named \"$CONTAINER\" already exists. Removing it ..."
-            docker rm $CONTAINER --force
-        elif [[ $(docker ps -f name=$CONTAINER | grep -e "[ \t]$CONTAINER\$") ]]; then
-            echo "ERROR: a container named \"$CONTAINER\" is already running"
-            echo "Please delete it and run this script again to continue a fresh installation!"
-            clean_up 1
-        else
-            echo "ERROR: a container named \"$CONTAINER\" exists but is not running"
-            echo "Please delete it and run this script again to continue a fresh installation!"
-            clean_up 1
-        fi
+
+    if [[ $USE_GIT_TOKEN == true ]]; then
+        echo "Git Access Token is required to build Docker image from: $TOOL_DOCKER_FILE"
+        echo -n "Enter your Git Access Token: "
+        read GIT_TOKEN
+        docker build -f $TOOL_DOCKER_FILE -t $TOOL_DOCKER_IMAGE --build-arg GIT_ACCESS_TOKEN=$GIT_TOKEN .
+    else
+        docker build -f $TOOL_DOCKER_FILE -t $TOOL_DOCKER_IMAGE .
     fi
 
-    # run your container
+    # Clear previous containers names if building for many tools
+    if [[ ${#ALL_TOOL_IDS[@]} > 1 ]]; then
+       CONTAINER_NAMES=()
+    fi
+
+    # Generate new container names
+    for ((i=1; i<=$NUM_CONTAINERS; i++)); do
+        CONTAINER_NAMES+=("$TOOL_ID-$i")
+    done
+
+    if [[ ${#CONTAINER_NAMES[@]} == 0 ]]; then
+        echo "Error: no container name or number of container is specified!"
+        print_run_help
+        exit 1
+    fi
+
+    # Create new Docker containers that share the two folders:
+    # `benchmarks` and `results` with the host system.
+    echo "============================================="
+    echo "Creating docker containers: ${CONTAINER_NAMES[*]}"
     echo ""
-    echo "Installing container \"$CONTAINER\" ..."
-    docker run -itd \
-        --name $CONTAINER \
-        -v $SMARTBENCH_BENCHMARKS_DIR:$DOCKER_BENCHMARKS_DIR \
-        -v $SMARTBENCH_RESULTS_DIR:$DOCKER_RESULTS_DIR \
-        $TOOL_DOCKER_IMAGE
+
+    if $FORCE_INSTALL; then
+        echo "Running force-install mode"
+    fi
+
+    for CONTAINER in "${CONTAINER_NAMES[@]}"; do
+        echo ""
+        echo "Checking container: $CONTAINER"
+        if [[ $(docker ps -a -f name=$CONTAINER | grep -e "[ \t]$CONTAINER\$") ]]; then
+            if $FORCE_INSTALL; then
+                echo "A container named \"$CONTAINER\" already exists. Removing it ..."
+                docker rm $CONTAINER --force
+            elif [[ $(docker ps -f name=$CONTAINER | grep -e "[ \t]$CONTAINER\$") ]]; then
+                echo "ERROR: a container named \"$CONTAINER\" is already running"
+                echo "Please delete it and run this script again to continue a fresh installation!"
+                clean_up 1
+            else
+                echo "ERROR: a container named \"$CONTAINER\" exists but is not running"
+                echo "Please delete it and run this script again to continue a fresh installation!"
+                clean_up 1
+            fi
+        fi
+
+        # run your container
+        echo ""
+        echo "Installing container \"$CONTAINER\" ..."
+        docker run -itd \
+            --name $CONTAINER \
+            -v $SMARTBENCH_BENCHMARKS_DIR:$DOCKER_BENCHMARKS_DIR \
+            -v $SMARTBENCH_RESULTS_DIR:$DOCKER_RESULTS_DIR \
+            $TOOL_DOCKER_IMAGE
+    done
+
+    # Clean after installation
+    clean_up 0
+
+    echo "Installation succeeded!"
+    echo "Run \"docker ps\" to see the newly launched Docker container "
 done
-
-# Clean after installation
-clean_up 0
-
-echo "Installation succeeded!"
-echo "Run \"docker ps\" to see the newly launched Docker container "
