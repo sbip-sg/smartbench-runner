517499c9654225fc130bf2f3a336f025c5117f5c
Ta Quang Trung <taquangtrungvn@gmail.com>
2023-05-01 16:38:38 +0800

    update script publishing SmartFuzz Docker

--- scripts/publish-smartfuzz-docker-image.sh
+++ scripts/publish-smartfuzz-docker-image.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
 
-echo "Preparing Docker image..."
+echo "Preparing SmartFuzz Docker image..."
 docker tag smartbench/smartfuzz taquangtrung/smartfuzz
 docker save taquangtrung/smartfuzz > docker_image_smartfuzz.tar
 
+echo -n "Enter your username in SBIP G2 to publish Smartfuzz Docker image: "
+read SBIP_G2_USER
+
 echo "Publishing Smartfuzz Docker image to SBIP G2..."
-scp docker_image_smartfuzz.tar  trung@sbip-g2.d2.comp.nus.edu.sg:/users/trung/share/docker/docker_image_smartfuzz.tar
+scp docker_image_smartfuzz.tar  $SBIP_G2_USER@sbip-g2.d2.comp.nus.edu.sg:/users/trung/share/docker/docker_image_smartfuzz.tar
