654ceba5569730731af2f11323499b5292b6160b
Ta Quang Trung <taquangtrungvn@gmail.com>
2023-05-03 01:07:02 +0800

    need to parse sfuzz output

--- smartbench/tools/sfuzz/sfuzz.py
+++ smartbench/tools/sfuzz/sfuzz.py
@@ -171,8 +171,8 @@ class Sfuzz(Tool):
     ):
         """Function to check whether an reported issue is related to a bug
         annotation."""
-        # Check for issue kind
-        return issue.issue_kind == annot.annot_issue_kind
+        # TODO: implement
+        return False
 
     def parse_instruction_coverage(self, test_output_dir: str):
         """Parse instruction coverage of sFuzz"""
