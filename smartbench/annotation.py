e7e34a0da4f764a6c88f17be8de1f6f061518e8a
thanhtoantnt <thanhtoantnt@gmail.com>
2023-05-14 10:34:52 +0800

    fix annotation parsing in smartbugs

--- smartbench/annotation.py
+++ smartbench/annotation.py
@@ -314,7 +314,7 @@ def parse_smartbugs_annotations(filename: str) -> List[BugAnnot]:
     bug_annots = []
     with open(filename, "r", encoding="utf-8") as file:
         for index, line in enumerate(file.readlines()):
-            start_line = index + 1
+            start_line = index + 2
             end_line = index + 2
             line = line.strip()
             if (
