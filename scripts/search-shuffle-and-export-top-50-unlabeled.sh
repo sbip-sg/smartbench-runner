7bdc2971cdb238c45e2656c0de0b698278571bf1
chen <chencassc@gmail.com>
2023-06-07 10:19:42 +0800

    add 50 shuffled unlabeled bugs for each type from realhacks

---
+++ scripts/search-shuffle-and-export-top-50-unlabeled.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+csv_folder="run3"
+
+# Array of bug types
+bug_types=(
+"Lack of Zero-Address Validation"
+"REENTRANCY,"
+"Integer Truncation"
+"Block Values Dependency \(BlockValueDependencySensitive\)"
+"Integer Overflow"
+"Integer Underflow"
+"REENTRANCY_READONLY"
+"ERC20 Access Control"
+"Access Control"
+"Unhandled Exception"
+"Unchecked Send Ether"
+"ERC20 Leak"
+"IntegerDivByZero by YUL"
+"BlockValueDependency,"
+"Authorization through tx.origin"
+"Arbitrary External Call"
+"Locking Ether"
+"Assertion Failure"
+"Denial of Service"
+"Unsafe DelegateCall"
+)
+
+# Loop over each bug type
+for bug_type in "${bug_types[@]}"; do
+    # Replace spaces and parentheses in the bug type with underscores to create a valid filename
+    filename=$(echo "$bug_type" | tr ' ' '_' | tr '(' '_' | tr ')' '_')
+
+    # Search for the bug type, remove blank lines, shuffle the results, take the top 50, and write to a file
+    rg -I -tcsv "$bug_type" "$csv_folder" | grep . | shuf | head -n 50 > "top_50_${filename}.csv"
+done
