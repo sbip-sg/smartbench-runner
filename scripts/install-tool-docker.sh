9aba083b06c1fdb70c66133121af05c0cd8c8cad
thanhtoantnt <thanhtoantnt@gmail.com>
2023-08-01 16:09:10 +0800

    add printout in script

--- scripts/install-tool-docker.sh
+++ scripts/install-tool-docker.sh
@@ -27,7 +27,7 @@ print_usage () {
     echo "Options:"
     echo "  -t <tool-id>              ID of analysis tool, currently support the followings:"
     echo "                              confuzzius, confuzzius-sbip, ilf, mythril, sfuzz,"
-    echo "                              slither, smartfuzz, smartian, verismart, efcf."
+    echo "                              slither, smartfuzz, smartian, verismart, efcf, ityfuzz."
     echo "                            Use `-t all` to install for all tools."
     echo "  -n <num_of_containers>    Number of containers to be installed, which are named"
     echo "                            as {tool-id}-1, {tool-id}-2,..., {tool-id}-n."
